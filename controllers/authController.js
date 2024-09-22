53fc1e1e0c7175f981c98d3f47feaf4ff9bc604b
Bharath <83769872+BharathKumar-c@users.noreply.github.com>
2024-09-23 00:25:34 +0530

    login controller and logics complete

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,4 +1,6 @@
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
+
 const users = require('../db/models/user');
 
 const generateToken = (payload) => {
@@ -46,4 +48,30 @@ const signup = async (req, res, next) => {
   });
 };
 
-module.exports = {signup};
+const login = async (req, res, next) => {
+  const {email, password} = req.body;
+  if ((!email, !password)) {
+    return res.status(400).json({
+      status: 'fail',
+      message: 'Please provid email and password',
+    });
+  }
+
+  const result = await users.findOne({where: {email}});
+
+  if (!result || !(await bcrypt.compare(password, result.password))) {
+    return res.status(400).json({
+      status: 'fail',
+      message: 'Incorrect email or password',
+    });
+  }
+
+  const token = generateToken({id: result.id});
+
+  return res.json({
+    status: 'success',
+    token,
+  });
+};
+
+module.exports = {signup, login};
